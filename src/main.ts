385deacbf312ebd743e9efb96489686f9a964b6a
axelkr <axelkr.dev@gmail.com>
2021-03-31 06:55:07 +0200

    setup so that CI pipeline runs successfully

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,5 @@ import { Logger } from 'sitka';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const loggerConfig: any = {name: 'accomplished-barnacle',level: Logger.Level.ALL};
+const logger = Logger.getLogger(loggerConfig);
+logger.info('started');
---
+++ test/sample.spec.ts
@@ -0,0 +1,5 @@
+describe('Sample', () => {
+	it('calculator', () => {
+		expect(1+1).toBe(2);
+	});
+});
