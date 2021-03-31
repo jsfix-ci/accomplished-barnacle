309f41f60fe4d657f440332d6a9e34626e2db969
axelkr <axelkr.dev@gmail.com>
2021-03-31 07:06:39 +0200

    start to get a CLI up and running

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+#!/usr/bin/env node
+
 import { Logger } from 'sitka';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const loggerConfig: any = {name: 'accomplished-barnacle',level: Logger.Level.ALL};
 const logger = Logger.getLogger(loggerConfig);
 logger.info('started');
+
+const argv = require('minimist')(process.argv.slice(2));
+console.log(argv);
