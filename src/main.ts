23cde042bf461641f20354b98ec3b21ec65c2dd6
Axel <axelkr.dev@google.com>
2021-04-01 06:20:56 +0200

    really start from scratch

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@
 import { Logger } from 'sitka';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const loggerConfig: any = {name: 'accomplished-barnacle',level: Logger.Level.ALL};
+const loggerConfig: any = { name: 'accomplished-barnacle', level: Logger.Level.ALL };
 const logger = Logger.getLogger(loggerConfig);
 logger.info('started');
-
-const argv = require('minimist')(process.argv.slice(2));
-console.log(argv);
