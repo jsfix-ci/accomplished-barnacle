dd7eadc0a6ba2b8e27ed1a6c6d807974b6d1f0f0
Axel <axelkr.dev@google.com>
2021-04-02 16:13:10 +0200

    more readable

--- src/Connectors/Configuration.ts
+++ src/Connectors/Configuration.ts
@@ -1,11 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 export abstract class Configuration {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
     abstract read(configuration: any): void;
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
     abstract validate(configuration: any): void;
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
     protected verifyThatConfigurationHasProperty(configuration: any, propertyName: string): void {
         // eslint-disable-next-line no-prototype-builtins
         if (!configuration.hasOwnProperty(propertyName)) {
