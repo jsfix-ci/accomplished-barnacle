6ffae337b1fd76182e366b128ace0d760ab85cef
Axel <axelkr.dev@google.com>
2021-04-02 07:18:48 +0200

    remove boilerplate

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { Logger } from 'sitka';
 import { Application } from './Application';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const loggerConfig: any = { name: 'accomplished-barnacle', level: Logger.Level.ALL };
+const loggerConfig: any = {
+    name: 'accomplished-barnacle', level: Logger.Level.ALL,
+    format: '[%{TIMESTAMP}] [%{LEVEL}] : %{MESSAGE}'
+};
 const logger = Logger.getLogger(loggerConfig);
 
 const application = new Application(logger);
