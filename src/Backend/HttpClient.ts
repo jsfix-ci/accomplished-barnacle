989557d6624892a923b240e23bd6a264bfecda51
Axel <axelkr.dev@google.com>
2021-04-02 09:26:19 +0200

    yikes. Connect more properly...

--- src/Backend/HttpClient.ts
+++ src/Backend/HttpClient.ts
@@ -1,4 +1,6 @@
-import http from 'https';
+import http from 'http';
+import url from 'url';
+
 import { Logger } from 'sitka';
 import { IHTTPClient } from 'prime-barnacle';
 import { Observable, Subject } from 'rxjs';
@@ -11,46 +13,47 @@ export class HttpClient implements IHTTPClient {
         this.logger = logger;
     }
 
-    postJson(url: string, json: ObjectEventREST): void {
-        this.logger.info('POST Json' + url + ' ' + json);
+    postJson(clientUrl: string, json: ObjectEventREST): void {
+        this.logger.info('POST Json' + clientUrl + ' ' + json);
         throw new Error('Method not implemented.');
     }
 
-    get(url: string): Observable<ObjectEventREST> {
-        this.logger.info('GET ' + url);
+    get(clientUrl: string): Observable<ObjectEventREST> {
+        this.logger.info('GET ' + clientUrl);
         const reporter = new Subject<ObjectEventREST>();
         const requestLogger = this.logger;
+        const urlObject = new URL(clientUrl);
 
-        http.get(url, (res) => {
-            const statusCode = res.statusCode;
-
-            if (statusCode !== 200) {
-                reporter.error(statusCode);
-                requestLogger.error(statusCode);
-                // consume response data to free up memory
-                res.resume;
-                return;
-            }
-
-            res.setEncoding('utf8');
-            let rawData: string;
-            res.on('data', (chunk) => rawData += chunk);
-            res.on('end', () => {
-                try {
-                    const parsedData = JSON.parse(rawData);
-                    requestLogger.info(rawData);
-                    reporter.next(parsedData);
-                    reporter.complete();
-
-                } catch (e) {
-                    requestLogger.error(e);
+        http.get(urlObject,
+            (res) => {
+                const statusCode = res.statusCode;
+                if (statusCode !== 200) {
                     reporter.error(statusCode);
+                    requestLogger.error(statusCode);
+                    // consume response data to free up memory
+                    res.resume;
+                    return;
                 }
+
+                res.setEncoding('utf8');
+                let rawData: string;
+                res.on('data', (chunk) => rawData += chunk);
+                res.on('end', () => {
+                    try {
+                        const parsedData = JSON.parse(rawData);
+                        requestLogger.info(rawData);
+                        reporter.next(parsedData);
+                        reporter.complete();
+
+                    } catch (e) {
+                        requestLogger.error(e);
+                        reporter.error(statusCode);
+                    }
+                });
+            }).on('error', (e) => {
+                requestLogger.error(e.message);
+                reporter.error(e.message);
             });
-        }).on('error', (e) => {
-            requestLogger.error(e.message);
-            reporter.error(e.message);
-        });
 
         return reporter;
     }
