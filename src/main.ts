1d35c0bf25cf8c3c3b8fe8304b6769e50f2de4bd
Axel <axelkr.dev@google.com>
2021-04-03 08:34:56 +0200

    easier to understand: just wrap everything into try-catch

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,21 @@ import { SettingsFactory } from './CommandLine/SettingsFactory';
 
 import { ConnectorFactory } from './Connectors/ConnectorFactory';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const loggerConfig: any = {
-    name: 'accomplished-barnacle', level: Logger.Level.INFO,
-    format: '[%{LEVEL}] : %{MESSAGE}'
-};
-const logger = Logger.getLogger(loggerConfig);
-
-const connectorFactory = new ConnectorFactory();
-const settings = SettingsFactory.generate(logger, connectorFactory);
-
 try {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const loggerConfig: any = {
+        name: 'accomplished-barnacle', level: Logger.Level.INFO,
+        format: '[%{LEVEL}] : %{MESSAGE}'
+    };
+    const logger = Logger.getLogger(loggerConfig);
+
+    const connectorFactory = new ConnectorFactory();
+    const settings = SettingsFactory.generate(logger, connectorFactory);
     const noProblems = settings.parseCommandLineArguments(process.argv);
     if (noProblems) {
         const application = new Application(settings, connectorFactory, logger);
         application.run();
     }
-
 } catch (e) {
     console.log(e.message);
 }
